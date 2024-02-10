316164af47544ff064d61e075f4e9daf42283d72
Alexej Harm <alexej.h@xiphos.de>
2024-02-10 10:28:28 +0100

    simplified configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,12 @@
+#include "main.hpp"
 #include <format>
-#include <random>
-#include <string>
-
-namespace application {
-
-std::string random()
-{
-  static thread_local std::random_device rd;
-  static thread_local std::uniform_int_distribution<std::size_t> dist(
-    0,
-    std::numeric_limits<std::size_t>::max());
-
-  std::string result;
-  for (auto i = 0; i < 1000; i++) {
-    result = std::format("{}", dist(rd));
-  }
-  return result;
-}
-
-}  // namespace application
+#include <cstdio>
+#include <cstdlib>
 
 int main(int argc, char* argv[])
 {
   try {
-    std::puts(std::format("{}\n", application::random()).data());
+    std::puts(std::format("{}", test()).data());
   }
   catch (const std::exception& e) {
     std::fputs(e.what(), stderr);
